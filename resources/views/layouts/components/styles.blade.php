04b67a879d5cd890981ca796fe21eac7b8c833fa
JessArthuro <jessarturo97@gmail.com>
2022-01-17 18:02:36 -0600

    se agrego la seccion about y services

--- resources/views/layouts/components/styles.blade.php
+++ resources/views/layouts/components/styles.blade.php
@@ -1,3 +1,5 @@
+<link rel="stylesheet" href="{{ asset('css/about.css') }}">
+<link rel="stylesheet" href="{{ asset('css/services.css') }}">
 <link rel="stylesheet" href="{{ asset('css/gallery.css') }}">
 <link rel="stylesheet" href="{{ asset('css/testimony.css') }}">
 <link rel="stylesheet" href="{{ asset('css/contact.css') }}">
--- resources/views/layouts/tours/pages/tour_six.blade.php
+++ resources/views/layouts/tours/pages/tour_six.blade.php
@@ -31,7 +31,7 @@
                     <div class="col-lg-6">
 
                         <div class="title">
-                            <h1 class="title-tour">Chichen Itza</h1>
+                            <h1 class="title-tour">coba - 3 cenotes</h1>
                             {{-- <a href="#">Reserva ahora</a> --}}
                         </div>
 
