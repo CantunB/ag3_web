7f3d3d8b6cfc278867c0a7b4d0f2c00f57611fb7
JessArthuro <jessarturo97@gmail.com>
2022-01-22 19:24:16 -0600

    correcciones finales

--- resources/views/layouts/components/services.blade.php
+++ resources/views/layouts/components/services.blade.php
@@ -1,10 +1,10 @@
-<section class="section mt-6 services" id="servicios">
+<section class="section mt-5 services" id="servicios">
     <div class="container">
         <h1 class="section-title text-center mb-4"><span>Servicios</span> que ofrecemos</h1>
 
         <div class="services-content">
             <div class="row">
-                <div class="col-md-2 col-lg-2 d-flex flex-column justify-content-between m-3">
+                <div class="col-md-6 col-lg-4 d-flex flex-column justify-content-between">
                     <img src="{{ asset('assets/images/airplane.svg') }}" alt="airplane">
                     <h4 class="text-center">Transportacion desde/hacia el aeropuerto</h4>
                     <p>Servicio de transportacion privada desde/hacia el aeropuerto.</p>
@@ -12,7 +12,7 @@
                         <a href="#">Ver servicio</a>
                     </div> --}}
                 </div>
-                <div class="col-md-2 col-lg-2 d-flex flex-column justify-content-between m-3">
+                <div class="col-md-6 col-lg-4 d-flex flex-column justify-content-between">
                     <img src="{{ asset('assets/images/hotel-hotel.svg') }}" alt="hotel-hotel">
                     <h4 class="text-center">Transportacion de hotel a hotel</h4>
                     <p>Servicio de transportacion privada de hotel a hotel.</p>
@@ -20,7 +20,7 @@
                         <a href="#">Ver servicio</a>
                     </div> --}}
                 </div>
-                <div class="col-md-2 col-lg-2 d-flex flex-column justify-content-between m-3">
+                <div class="col-md-6 col-lg-4 d-flex flex-column justify-content-between">
                     <img src="{{ asset('assets/images/open-transportation.svg') }}" alt="open-transportation">
                     <h4 class="text-center">Transportacion abierta</h4>
                     <p>Viaje a su propio ritmo con nuestra transportacion abierta.</p>
@@ -28,7 +28,7 @@
                         <a href="#">Ver servicio</a>
                     </div> --}}
                 </div>
-                <div class="col-md-2 col-lg-2 d-flex flex-column justify-content-between m-3">
+                <div class="col-md-6 col-lg-6 d-flex flex-column justify-content-between">
                     <img src="{{ asset('assets/images/transfer.svg') }}" alt="transfer">
                     <h4 class="text-center">Transportacion de traslado</h4>
                     <p>Servicio de transportacion a su medida con descuentos especiales.</p>
@@ -36,8 +36,8 @@
                         <a href="#">Ver servicio</a>
                     </div> --}}
                 </div>
-                <div class="col-md-2 col-lg-2 d-flex flex-column justify-content-between m-3">
-                    <img src="{{ asset('assets/images/wedding.svg') }}" alt="transfer">
+                <div class="col-md-12 col-lg-6 d-flex flex-column justify-content-between">
+                    <img src="{{ asset('assets/images/wedding2.svg') }}" alt="transfer">
                     <h4 class="text-center">Transportacion a tu evento</h4>
                     <p>Servicio de transportacion a eventos  especiales.</p>
                     {{-- <div class="btn">
